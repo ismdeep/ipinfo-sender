3cc1d68bd5789e8a9f890cc178819ace0c8e77d3
L. Jiang <l.jiang.1024@gmail.com>
2021-03-22 13:25:30 +0800

    feat: Add config.json Support

--- sender/help_msg.go
+++ sender/help_msg.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func ShowHelpMsg() {
-	fmt.Println("Usage: ipinfo-sender    -c <config.json path>    [-t]")
-	fmt.Println("    -t    Loop forever")
+	fmt.Println("Usage: ipinfo-sender    -c <config.json path>")
 	fmt.Println("    -c <config.json path>    config.json file path")
+	fmt.Println()
+	fmt.Println("    config.example.json shows at https://raw.githubusercontent.com/ismdeep/ipinfo-sender/main/config.example.json")
 }
